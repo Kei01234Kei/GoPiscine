1acad0f92a316fe9991ec3a39bf2229cf79325be
Keisuke Ishigami <keisuke.130@icloud.com>
2022-05-15 06:29:49 +0900

    ex07完成

---
+++ Go02/ex07/vendor/piscine/findnextprime.go
@@ -0,0 +1,20 @@
+package piscine
+
+func isPrime(nb int) bool {
+	if nb < 2 {
+		return false
+	}
+	for i := 2; i <= nb/i; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func FindNextPrime(nb int) int {
+	for !isPrime(nb) {
+		nb++
+	}
+	return nb
+}
