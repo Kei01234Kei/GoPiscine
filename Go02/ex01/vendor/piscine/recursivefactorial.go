b14761913c0e6a197766996d32c27c5d448e41af
Keisuke Ishigami <keisuke.130@icloud.com>
2022-05-15 04:12:23 +0900

    オーバーフロー対策を追加

--- Go02/ex01/vendor/piscine/recursivefactorial.go
+++ Go02/ex01/vendor/piscine/recursivefactorial.go
@@ -3,11 +3,17 @@ package piscine
 func RecursiveFactorial(nb int) int {
 	if nb < 0 {
 		return 0
-	}
-	if nb == 0 {
+	} else if 0 <= nb && nb <= 1 {
 		return 1
 	}
+	intMax := int(^uint(0) >> 1)
 	num := 1
+	if intMax/(nb-1) < nb {
+		return 0
+	}
 	num = nb * RecursiveFactorial(nb-1)
+	if intMax/nb < num {
+		return 0
+	}
 	return num
 }
