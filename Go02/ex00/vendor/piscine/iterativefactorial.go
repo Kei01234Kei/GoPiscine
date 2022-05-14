fbee8d0e444ae02f0a074c9e971bbaa710001c9c
Keisuke Ishigami <keisuke.130@icloud.com>
2022-05-15 03:34:30 +0900

    オーバーフロー対策を実装

--- Go02/ex00/vendor/piscine/iterativefactorial.go
+++ Go02/ex00/vendor/piscine/iterativefactorial.go
@@ -4,8 +4,12 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
+	intMax := int(^uint(0) >> 1)
 	num := 1
 	for nb > 0 {
+		if intMax/nb < num {
+			return 0
+		}
 		num *= nb
 		nb--
 	}
