2830dd07440da6e4f0885c386fb72fadb85b60d7
Keisuke Ishigami <keisuke.130@icloud.com>
2022-05-18 09:48:14 +0900

    ex05のコードを微修正

--- Go03/ex05/vendor/piscine/toupper.go
+++ Go03/ex05/vendor/piscine/toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	rs := []rune(s)
 	for i, _ := range rs {
 		if 'a' <= rs[i] && rs[i] <= 'z' {
-			rs[i] = 'A' - 'a' + rs[i]
+			rs[i] = rs[i] + ('A' - 'a')
 		}
 	}
 	return string(rs)
